719e0f001b008cd09319ab8d167694c805f78e1f
Stefan989732 <stefan.wurpel@hotmail.com>
2026-02-13 11:31:17 +0100

    small change to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 <div className='p-3'>
     <div className="bg-white shadow-lg rounded-xl p-6 border padding-4">
   <h1 className="rounded-xl w-full h-150 object-cover rounded-t-xl text-gray-600">Stefan Wurpel</h1>
-  <h3 className="text-xl font-bold mt-4 mb-2 text-gray-600">Titel</h3>
+  <h3 className="text-xl font-bold mt-4 mb-2 text-gray-600">Welkom</h3>
   <p className="text-gray-600 mb-4">Korte beschrijving van de inhoud.</p>
   <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">Lees meer</button>
   </div>
