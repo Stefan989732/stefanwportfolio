2883529cd1bd7da72f30d2b3aceb320445e6c68c
Stefan989732 <stefan.wurpel@hotmail.com>
2026-02-25 15:20:53 +0100

    added projects & some more styling

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <div className="grid pt-6 justify-items-center grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
           <div>
             <div className="bg-white rounded-xl  border-t-4 border-[var(--accent)] p-6 shadow-xl/60 border-2">
-              <h1 className="text-4xl text-center border-b-4 pb-4 mb-6">Frontend Skills</h1>
+              <h1 className="text-4xl text-center border-b-4 border-[var(--accent)] pb-4 mb-6">Frontend Skills</h1>
               <div className="flex flex-wrap gap-6 pt-6 justify-center gap-4 text-4xl text-black p-3">
                 <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
                   <div className="flex flex-col items-center gap-2 h-full pb-4">
@@ -82,7 +82,7 @@ export default function Home() {
           </div>
           <div>
             <div className="bg-white rounded-xl border-t-4 border-[var(--accent)] p-6 shadow-xl/60 border-2">
-              <h1 className="text-4xl text-center border-b-4 pb-4 mb-6">Backend Skills</h1>
+              <h1 className="text-4xl text-center border-b-4 border-[var(--accent)] pb-4 mb-6">Backend Skills</h1>
               <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
                 <div className="flex flex-col items-center gap-2 h-full pb-4">
                   <FaNodeJs title="Node.js" className="text-[#3C873A] h-16 w-16" />
@@ -113,7 +113,7 @@ export default function Home() {
           </div>
           <div>
             <div className="bg-white rounded-xl max-w-sm border-t-4 border-[var(--accent)] p-6 shadow-xl/60 border-2">
-              <h1 className="text-4xl text-center border-b-4 pb-4 mb-6">Full-stack</h1>
+              <h1 className="text-4xl text-center border-b-4 border-[var(--accent)] pb-4 mb-6">Full-stack</h1>
               <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
                 <div className="flex flex-col items-center gap-2 h-full pb-4">
                   <SiCplusplus className="h-16 w-16 text-[blue]" />
@@ -130,17 +130,17 @@ export default function Home() {
         </div>
         <div className="flex items-center justify-center text-4xl text-white gap-2"><Divider />Projecten<Divider /></div>
         <div className="flex flex-wrap gap-6 pt-6 justify-center">
-          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--detail)] bg-white rounded-xl border-2">
+          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--accent)] bg-white rounded-xl border-2">
             <a href="#"><img className="rounded-t-xl w-full" src="/verpleegkundeGame.png" alt="" /></a>
             <p className="mt-4">verpleegkunde game (C#)</p>
           </div>
 
-          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--detail)] bg-white rounded-xl border-2">
+          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--accent)] bg-white rounded-xl border-2">
             <a href="#"><img className="rounded-t-xl w-full" src="/StefanWurpel.jpg" alt="" /></a>
             <p className="mt-4">ITP Generator (Vue.js)</p>
           </div>
 
-          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--detail)] bg-white rounded-xl border-2">
+          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--accent)] bg-white rounded-xl border-2">
             <a href="#"><img className="rounded-t-xl w-full" src="/StefanWurpel.jpg" alt="" /></a>
             <p className="mt-4">Enovation</p>
           </div>
