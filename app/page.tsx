1037999f5ba1b66290043a32abbb04c9530e1db2
Stefan989732 <stefan.wurpel@hotmail.com>
2026-02-25 15:17:01 +0100

    added projects & some more styling

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div>
       <div className='px-3 text-black'>
-        <div className="grid grid-cols-2 bg-white rounded-xl shadow-xl/30">
+        <div className="grid grid-cols-2 bg-white rounded-xl shadow-xl/30 border-2 border-t-4 border-[var(--accent)]">
 
           <div className='justify-center grid rounded-xl p-6 padding-4 grid' >
             <Image src={'/ProfilePicture.jpg'} className="mask-radial-at-center mask-radial-from-50% mask-radial-to-70%" />
@@ -27,110 +27,129 @@ export default function Home() {
           <div className='col-span-full className="bg-black rounded-xl p-6 w-full grid"'>
             {/* <p>to do:</p>
             <ul>
-              <li>1 introductie</li>
-              <li>2 hobbies</li>
-              <li>3 vaardigheden</li>
-              <li>4 call to action</li>
+              <li>1 introductie X</li>
+              <li>2 hobbies X</li>
+              <li>3 vaardigheden X</li>
+              <li>4 call to action X</li>
               <li>5 hoofd projecten</li>
             </ul> */}
           </div>
         </div><br />
-        <div className="flex items-center justify-center text-4xl text-white"><Divider className="" />Skills<Divider /></div>
-        <div className="grid pt-6 gap-6 justify-items-center grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+        <div className="flex items-center justify-center text-4xl text-white gap-2"><Divider />Skills<Divider /></div>
+        <div className="grid pt-6 justify-items-center grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
           <div>
-            <div className="bg-white rounded-xl  border-[var(--accent)] ">
-              <div className="col-span-3 border-b-4 text-center p-3"><h1 className="text-2xl">Frontend Skills</h1></div>
-              <div className="flex flex-wrap gap-4 text-4xl text-black p-3">
-                <div className="grid grid-cols-3 gap-8 justify-items-center">
-                  <div className="flex flex-col items-center gap-3">
-                    <SiNextdotjs title="Next.js" className="h-24 w-24 text-[black]" />
-                    <p>Next.js</p>
+            <div className="bg-white rounded-xl  border-t-4 border-[var(--accent)] p-6 shadow-xl/60 border-2">
+              <h1 className="text-4xl text-center border-b-4 pb-4 mb-6">Frontend Skills</h1>
+              <div className="flex flex-wrap gap-6 pt-6 justify-center gap-4 text-4xl text-black p-3">
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <SiNextdotjs title="Next.js" className="h-16 w-16 text-[black]" />
+                    <p className="text-2xl">Next.js</p>
                   </div>
 
-                  <div className="flex flex-col items-center gap-3">
-                    <SiTypescript title="TypeScript" className="text-[#3178C6] h-24 w-24" />
-                    <p>TypeScript</p>
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <SiTypescript title="TypeScript" className="text-[#3178C6] h-16 w-16" />
+                    <p className="text-2xl">TypeScript</p>
                   </div>
 
-                  <div className="flex flex-col items-center gap-3">
-                    <SiTailwindcss title="Tailwind CSS" className="text-[#38bdf8] h-24 w-24" />
-                    <p>TailwindCSS</p>
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <SiTailwindcss title="Tailwind CSS" className="text-[#38bdf8] h-16 w-16" />
+                    <p className="text-2xl">TailwindCSS</p>
                   </div>
 
-                  <div className="flex flex-col items-center gap-3">
-                    <SiJavascript className="h-24 w-24 text-[yellow]" />
-                    <p>JavaScript</p>
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <SiJavascript className="h-16 w-16 text-[yellow]" />
+                    <p className="text-2xl">JavaScript</p>
                   </div>
 
-                  <div className="flex flex-col items-center gap-3">
-                    <FaHtml5 className="h-24 w-24 text-[orange]" />
-                    <p>HTML</p>
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <FaHtml5 className="h-16 w-16 text-[orange]" />
+                    <p className="text-2xl">HTML</p>
                   </div>
 
-                  <div className="flex flex-col items-center gap-3">
-                    <FaSass className="h-24 w-24 text-[pink]" />
-                    <p>SCSS</p>
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <FaSass className="h-16 w-16 text-[pink]" />
+                    <p className="text-2xl">SCSS</p>
                   </div>
 
-                  <div className="flex flex-col items-center gap-3">
-                    <FaAngular className="h-24 w-24 text-[red]" />
-                    <p>Angular</p>
+                  <div className="flex flex-col items-center gap-2 h-full pb-4">
+                    <FaAngular className="h-16 w-16 text-[red]" />
+                    <p className="text-2xl">Angular</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div>
-            <div className="bg-white rounded-xl border-[var(--accent)] block">
-              <div className="col-span-3 border-b-4 text-center"><h1 className="text-2xl p-3">Backend Skills</h1></div>
-              <div className="grid grid-cols-3 gap-8 justify-items-center p-3 text-4xl text-black">
-                <div className="flex flex-col items-center gap-3">
-                  <FaNodeJs title="Node.js" className="text-[#3C873A] h-24 w-24" />
-                  <p>Node.js</p>
+            <div className="bg-white rounded-xl border-t-4 border-[var(--accent)] p-6 shadow-xl/60 border-2">
+              <h1 className="text-4xl text-center border-b-4 pb-4 mb-6">Backend Skills</h1>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <FaNodeJs title="Node.js" className="text-[#3C873A] h-16 w-16" />
+                  <p className="text-2xl">Node.js</p>
                 </div>
 
-                <div className="flex flex-col items-center gap-3">
-                  <FaLaravel className="h-24 w-24 text-[red]" />
-                  <p>Laravel</p>
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <FaLaravel className="h-16 w-16 text-[red]" />
+                  <p className="text-2xl">Laravel</p>
                 </div>
 
-                <div className="flex flex-col items-center gap-3">
-                  <SiMysql className="h-24 w-24 text-[black]" />
-                  <p>MySQL</p>
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <SiMysql className="h-16 w-16 text-[black]" />
+                  <p className="text-2xl">MySQL</p>
                 </div>
 
-                <div className="flex flex-col items-center gap-3">
-                  <TbBrandCSharp className="h-24 w-24 text-[purple]" />
-                  <p>C#</p>
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <TbBrandCSharp className="h-16 w-16 text-[purple]" />
+                  <p className="text-2xl">C#</p>
                 </div>
 
-                <div className="flex flex-col items-center gap-3">
-                  <FaJava className="h-24 w-24 text-[black]" />
-                  <p>Java</p>
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <FaJava className="h-16 w-16 text-[black]" />
+                  <p className="text-2xl">Java</p>
                 </div>
               </div>
             </div>
           </div>
           <div>
-            <div className="bg-white rounded-xl max-w-sm border-[var(--accent)] block">
-              <div className="col-span-3 border-b-4 text-center"><h1 className="text-2xl p-3">Frontend Skills</h1></div>
-              <div className="grid grid-cols-3 gap-8 justify-items-center p-3 text-4xl text-black">
-                <div className="flex flex-col items-center gap-3">
-                  <SiCplusplus className="h-24 w-24 text-[blue]" />
-                  <p>C++</p>
+            <div className="bg-white rounded-xl max-w-sm border-t-4 border-[var(--accent)] p-6 shadow-xl/60 border-2">
+              <h1 className="text-4xl text-center border-b-4 pb-4 mb-6">Full-stack</h1>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <SiCplusplus className="h-16 w-16 text-[blue]" />
+                  <p className="text-2xl">C++</p>
                 </div>
 
-                <div className="flex flex-col items-center gap-3">
-                  <FaUnity className="h-24 w-24 text-[black]" />
-                  <p>Unity</p>
+                <div className="flex flex-col items-center gap-2 h-full pb-4">
+                  <FaUnity className="h-16 w-16 text-[black]" />
+                  <p className="text-2xl">Unity</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <br /><div className="flex items-center justify-center text-white"><Divider className="h-px" /><h1 className="flex justify-center text-4xl">Hobby's</h1><Divider /></div>
+        <div className="flex items-center justify-center text-4xl text-white gap-2"><Divider />Projecten<Divider /></div>
+        <div className="flex flex-wrap gap-6 pt-6 justify-center">
+          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--detail)] bg-white rounded-xl border-2">
+            <a href="#"><img className="rounded-t-xl w-full" src="/verpleegkundeGame.png" alt="" /></a>
+            <p className="mt-4">verpleegkunde game (C#)</p>
+          </div>
+
+          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--detail)] bg-white rounded-xl border-2">
+            <a href="#"><img className="rounded-t-xl w-full" src="/StefanWurpel.jpg" alt="" /></a>
+            <p className="mt-4">ITP Generator (Vue.js)</p>
+          </div>
+
+          <div className="max-w-sm p-6 text-center border-t-4 border-[var(--detail)] bg-white rounded-xl border-2">
+            <a href="#"><img className="rounded-t-xl w-full" src="/StefanWurpel.jpg" alt="" /></a>
+            <p className="mt-4">Enovation</p>
+          </div>
+        </div>
+
+
+        <br /><div className="flex items-center justify-center text-white gap-2"><Divider /><h1 className="flex justify-center text-4xl">Hobby's</h1><Divider /></div>
         <div className="grid pt-6 gap-6 justify-items-center grid-cols-1 sm:grid-cols-2 xl:grid-cols-4">
-          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)]">
+          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)] shadow-xl/30 border-t-4 border-[var(--accent)]">
             <a href="#">
               <img className="rounded-t-xl" src="/StefanWurpel.jpg" alt="" />
             </a>
@@ -148,7 +167,7 @@ export default function Home() {
               <li>puzzle games</li>
             </ul> */}
           </div>
-          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)]">
+          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)] shadow-xl/30 border-t-4 border-[var(--accent)]">
             <a href="#">
               <img className="rounded-t-xl" src="/StefanWurpel.jpg" alt="" />
             </a>
@@ -166,7 +185,7 @@ export default function Home() {
               <li>calisthenics/home gym</li>
             </ul> */}
           </div>
-          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)]">
+          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)] shadow-xl/30 border-t-4 border-[var(--accent)]">
             <a href="#">
               <img className="rounded-t-xl" src="/StefanWurpel.jpg" alt="" />
             </a>
@@ -192,7 +211,7 @@ export default function Home() {
               <li>mentale gezondheid</li>
             </ul>
           </div> */}
-          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)]">
+          <div className="block max-w-sm border border-[var(--accent)] border-default rounded-xl shadow-xs bg-[var(--accent)] shadow-xl/30 border-t-4 border-[var(--accent)]">
             <a href="#">
               <img className="rounded-t-xl" src="/StefanWurpel.jpg" alt="" />
             </a>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function projects() {
     return(
         <div>
         <div className='px-3'>
-        <div className="bg-black shadow-lg rounded-xl p-6 padding-4 w-full h-150 grid grid-cols-2 gap-4">
+        <div className="bg-white shadow-lg rounded-xl p-6 padding-4 w-full h-150 grid grid-cols-2 gap-4">
           <div>
             <h1 className="text-gray-600 justify-center flex text-2xl">Stefan Wurpel</h1><br />
             <p className='text-gray-600 justify-center flex'>Welcome op mijn portfolio pagina</p>
